b5bc5a513ffc95d6873216a25e79054c461369f0
riorz <reevizy@gmail.com>
2016-12-09 02:03:04 +0900

    unlink file after converting. change the ffmpeg command slightly.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const exec = require('child_process').exec;
 
 var vid = ( process.argv[2] != null )? process.argv[2] : null ;
 //convert to mp3 using ffmpeg
-var flv2mp3 = `ffmpeg -y -i temp.flv -vn -acodec libmp3lame -ac 2 -ab 192000 -ar 44100 -threads 4 -f mp3 `;
+var flv2mp3 = `ffmpeg -y -i temp.flv -acodec libmp3lame -ac 2 -ab 192000 -ar 44100 -threads 4 -f mp3 `;
 
 // download file if there is a vid.
 if( vid !== null ){
@@ -15,10 +15,14 @@ if( vid !== null ){
         .on('info', (info,format)=>{ flv2mp3 += ` '${info.title}.mp3'`;})
         .pipe(dlStream)
         .on('finish',()=>{
-        console.log('download over');
-        exec( flv2mp3, (err,stdout,stderr)=>{
-            if(err) throw err;
-            else console.log('conver complete.');
+            console.log('download over');
+            //execute ffmpeg command
+            exec( flv2mp3, (err,stdout,stderr)=>{
+                if(err) throw err;
+                else console.log('convert file completed.');
+                })
+                .on('exit',()=>{    //remove temporary file while file is converted.
+                    fs.unlink('./temp.flv',(err)=>{ if(err) throw err;});
+                });
         });
-    });
 }
